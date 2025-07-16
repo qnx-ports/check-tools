ff67281b1686626f7a5d4b7d7414faf4d34d3110
Ethan Leir <eleir@blackberry.com>
2025-07-16 12:31:09 -0400

    Fix check.sh.

--- bin/check.sh
+++ bin/check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 python3 $SCRIPT_DIR/check.py $@
